2d4f8b4b4902f354f91d9f118c08b6bf28d05a88
Viki <hi@viki.moe>
2026-08-16 14:06:49 +0800

    chore: save

--- cf-worker.ts
+++ cf-worker.ts
@@ -1,8 +1,8 @@
 // @ts-ignore
 import { httpServerHandler } from 'cloudflare:node'
 
-import('./app.ts')
+import './app.ts'
 
-const port = process.env.PORT || 3000
-
-export default httpServerHandler({ port })
+export default httpServerHandler({
+  port: process.env.PORT || 3000,
+})
